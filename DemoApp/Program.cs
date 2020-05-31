45dd7eae6c185151d8750917e843290d1985c3c3
Sean Garrett <sean@blacktau.com>
2020-05-31 22:39:20 +0100

    fixed DemoApp references to load project ref

--- DemoApp/Program.cs
+++ DemoApp/Program.cs
@@ -1,12 +1,8 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
 using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Blazor.Essentials;
 
 namespace DemoApp
@@ -20,8 +16,10 @@ namespace DemoApp
 
             builder.Services.AddTransient(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
             builder.Services.AddBlazorEssentials();
-
+            
             await builder.Build().RunAsync();
         }
+
+
     }
 }
